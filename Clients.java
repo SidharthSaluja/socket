e67bba455b0a3f91bf95f766c7435c054d99b1c8
Sidharth Saluja <67796130+SidharthSaluja@users.noreply.github.com>
2020-08-15 11:51:35 -0500

    Add files via upload

---
+++ Clients.java
@@ -0,0 +1,86 @@
+import java.net.*; 
+import java.io.*; 
+  
+public class Clients 
+{ 
+    // initialize socket and input output streams 
+    private Socket socket            = null; 
+    private DataInputStream  input   = null; 
+    private DataOutputStream out     = null; 
+  
+    // constructor to put ip address and port 
+    public Clients(String address, int port) 
+    { 
+        // establish a connection 
+        try
+        { 
+            socket = new Socket(address, port); 
+            System.out.println("Connected"); 
+  
+            // takes input from terminal 
+            input  = new DataInputStream(System.in); 
+  
+            // sends output to the socket 
+            out    = new DataOutputStream(socket.getOutputStream()); 
+        } 
+        catch(UnknownHostException u) 
+        { 
+            System.out.println(u); 
+        } 
+        catch(IOException i) 
+        { 
+            System.out.println(i); 
+        } 
+  
+        // string to read message from input 
+        String line = ""; 
+     String u="";
+        // keep reading until "Over" is input 
+        while (!line.equals("Over")) 
+        { 
+            try
+            { 
+                line = input.readLine(); 
+                out.writeUTF(line); 
+            } 
+            catch(IOException i) 
+            { 
+                System.out.println(i); 
+            } 
+        }  
+
+
+
+   while (!u.equals("Over")) 
+            { 
+                try
+                { 
+                    u = input.readUTF(); 
+                    System.out.println(u); 
+  
+                } 
+                catch(IOException i) 
+                { 
+                    System.out.println(i); 
+                } 
+            } 
+
+  
+        // close the connection 
+        try
+        { 
+            input.close(); 
+            out.close(); 
+            socket.close(); 
+        } 
+        catch(IOException i) 
+        { 
+            System.out.println(i); 
+        } 
+    } 
+  
+    public static void main(String args[]) 
+    { 
+        Clients client = new Clients("127.0.0.1", 5000); 
+    } 
+} 
